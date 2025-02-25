00c352b9979d80707ef4d9c7f7d27055d4bc1aef
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-02-25 15:53:03 +0200

    feat(auth): integrate csrf protection and user auth with inertia
    - include `InertiaCsrf` module in `ApplicationController`
    - add inertia_share to provide authentication data
    - share current user email when user is signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  include InertiaCsrf
+
+  inertia_share auth: -> {
+                  if user_signed_in?
+                    {
+                      user: current_user.email,
+                    }
+                  end
+                }
 end
