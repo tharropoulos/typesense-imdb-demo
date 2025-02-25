85c487c9b5902e795c91b8809fee59568fd8ed9f
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-02-25 13:34:00 +0200

    chore: configure prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,31 @@
+import { fileURLToPath } from "url";
+
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+const config = {
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 120,
+  experimentalTernaries: true,
+  importOrder: [
+    "<TYPES>",
+    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+    "^(next/(.*)$)|^(next$)",
+    "^(expo(.*)$)|^(expo$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "<TYPES>^@ena",
+    "",
+    "<TYPES>^[.|..|~]",
+    "^~/",
+    "^[../]",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "5.6.0",
+};
+
+export default config;
