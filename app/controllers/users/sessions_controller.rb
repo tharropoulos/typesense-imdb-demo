b0c23c261731da80d8390664266f61244a5f58bc
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-02-26 12:34:19 +0200

    fix(auth): improve user session authentication flow
    - Refactor `new` method to remove duplicate rendering
    - Customize `create` method for manual authentication
    - Add custom error handling for invalid credentials
    - Implement `after_sign_in_path_for` and `after_sign_out_path_for` methods

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,25 +1,34 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
-  # GET /login
+  layout "application"
+
+  respond_to :html, :json
+
   def new
-    render inertia: "Auth/Login", props: {}
+    self.resource = resource_class.new
+    render inertia: "Auth/Login"
   end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = warden.authenticate(auth_options)
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+    if resource
+      sign_in(resource_name, resource)
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      render inertia: "Auth/Login", props: {
+        errors: { base: ["Invalid email or password"] },
+        values: { email: params.dig(:user, :email) },
+      }, status: :unprocessable_entity
+    end
+  end
 
-  # protected
+  protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
