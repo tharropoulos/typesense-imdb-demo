7b8a72046a17a6fde6dccc4173c2709300b4f30b
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-02-26 15:15:42 +0200

    feat(model): implement Genre model with associations
    - Create bidirectional relationships with movies through `movie_genres`
    - Create bidirectional relationships with tv shows through `tv_show_genres`
    - Add validation to ensure genre names are unique
    - Configure dependent destroy for join tables

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,9 @@
 class Genre < ApplicationRecord
+  has_many :movie_genres, dependent: :destroy
+  has_many :movies, through: :movie_genres
+
+  has_many :tv_show_genres, dependent: :destroy
+  has_many :tv_shows, through: :tv_show_genres
+
+  validates :name, presence: true, uniqueness: true
 end
