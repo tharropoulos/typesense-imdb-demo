243f3062e3c1db9f298eb6cf4e931b578ded24b9
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-02-27 16:15:09 +0200

    feat(model): add validations to `country` model
    - add presence and uniqueness validations for `name` attribute
    - ensure data integrity for `country` records

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,6 @@
 class Country < ApplicationRecord
+  has_many :movie_countries, dependent: :destroy
+  has_many :movies, through: :movie_countries
+
+  validates :name, presence: true, uniqueness: true
 end
