eac17524a8bc987f9da5a156573cc6edb03c5435
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-02-28 12:06:48 +0200

    feat(model): add tv show relations to country model

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,5 +2,8 @@ class Country < ApplicationRecord
   has_many :movie_countries, dependent: :destroy
   has_many :movies, through: :movie_countries
 
+  has_many :tv_show_countries, dependent: :destroy
+  has_many :tv_shows, through: :tv_show_countries
+
   validates :name, presence: true, uniqueness: true
 end
