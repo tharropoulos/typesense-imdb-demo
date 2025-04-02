cb8815907d6ce529c0bf8a50e1debd4b02d74197
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-04-02 12:16:28 +0300

    fix(search): improve typesense configuration with logging
    - Add logger setup to output to stdout
    - Set log level to DEBUG
    - Fix formatting of nodes configuration array
    - Maintain connection timeout setting

--- config/initializers/typesense.rb
+++ config/initializers/typesense.rb
@@ -3,7 +3,10 @@ Typesense.configuration = {
     host: ENV["TYPESENSE_HOST"] || "localhost",
     port: ENV["TYPESENSE_PORT"] || 8108,
     protocol: ENV["TYPESENSE_PROTOCOL"] || "http",
-  }],
-  api_key: ENV["TYPESENSE_API_KEY"] || "xyz", # Replace with your actual API key
+    },
+  ],
+  api_key: ENV["TYPESENSE_API_KEY"] || "xyz",
   connection_timeout_seconds: 2,
+  logger: Logger.new($stdout),
+  log_level: Logger::DEBUG,
 }
