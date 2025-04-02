e381c6718242177c159eb30b087fbd121b2f64bd
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-04-02 13:10:16 +0300

    feat(ui): add breadcrumbs component

---
+++ app/frontend/components/breadcrumbs.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Link } from "@inertiajs/react";
+
+export function BreadCrumbNav({
+  items,
+}: {
+  items: {
+    url: string;
+    label: string;
+  }[];
+}) {
+  if (!items || items.length === 0) return null;
+
+  return (
+    <div className="mb-4">
+      <Breadcrumb>
+        <BreadcrumbList>
+          {items.map((item, index) => (
+            <React.Fragment key={`${item.label}-${index}-fragment`}>
+              <BreadcrumbItem key={`${item.label}-${index}-link`}>
+                <BreadcrumbLink asChild>
+                  <Link href={item.url}>{item.label}</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              {/* Add separator after each item except the last one */}
+              {index < items.length - 1 && <BreadcrumbSeparator />}
+            </React.Fragment>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+}
