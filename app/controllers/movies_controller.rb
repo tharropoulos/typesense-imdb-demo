6248e206955c03114fe6bc4847a0b286bb89bd81
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-04-02 12:36:29 +0300

    feat(movies): implement movie suggestions algorithm in typesense
    - Add `suggestions` method to generate movie recommendations
    - Create two recommendation strategies: movies from same director and genre-based similar movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,11 +6,14 @@ class MoviesController < ApplicationController
     all_genres = movie.genres.map { |genre| genre[:name] }.compact
 
     primary_director = movie.movie_directors.first.person_id
+    suggestions = suggestions(movie.id, primary_director, all_genres)
+
     add_breadcrumb("Movies", "/movies")
     add_breadcrumb(movie.title)
 
     render inertia: "Movie/Show", props: {
              movie: MoviePresenter.new(movie).as_json,
+             suggestions: suggestions,
              breadcrumbs: @breadcrumbs,
            }
   end
@@ -19,6 +22,41 @@ class MoviesController < ApplicationController
     add_breadcrumb("Movies", "/movies")
     render inertia: "Movie/Index", props: {
       breadcrumbs: @breadcrumbs,
+    }
+  end
+
+  private
+
+  def suggestions(current_movie_id, director_id, genres, limit = 6)
+    genre_filter = "genres:[#{genres.join(", ")}]"
+    primary_genre = genres.first
+    secondary_genre = genres.size > 1 ? genres.second : nil
+
+    sort_by = if genres.size > 1
+        "_eval([((primary_genre:#{primary_genre} && secondary_genre:#{secondary_genre} && num_votes:>10000) || (primary_genre:#{secondary_genre} && secondary_genre:#{primary_genre} && num_votes:>10000)):4, (average_rating:>8.0 && num_votes:>10000 && primary_genre:#{primary_genre}):3, (average_rating:>8.0 && secondary_genre:#{secondary_genre}):2 ]):desc"
+      else
+        "_eval([(average_rating:>8.0 && num_votes:>10000 && primary_genre:#{primary_genre}):3]):desc"
+      end
+    from_director_params = {
+      filter_by: "$MovieDirector(person_id: #{director_id}) && id:!=#{current_movie_id}",
+      sort_by: "average_rating:desc, num_votes:desc, release_year:desc",
+      include_fields: "title, average_rating, primary_image_url, release_year, id, genres, ",
+      per_page: 15,
+    }
+
+    similar_to_params = {
+      filter_by: "#{genre_filter} && id:!=#{current_movie_id}",
+      sort_by: "#{sort_by}, average_rating:desc, num_votes:desc",
+      include_fields: "title, average_rating, primary_image_url, release_year, id, genres, collection_type",
+      per_page: 15,
+    }
+
+    from_director = Movie.raw_search("*", "", from_director_params)["hits"].map { |hit| hit["document"] }
+    similar_to = Movie.raw_search("*", "", similar_to_params)["hits"].map { |hit| hit["document"] }
+
+    {
+      from_director: from_director,
+      similar_to: similar_to,
     }
   end
 end
