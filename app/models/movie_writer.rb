0c9fca25001195d13a093fd2e1400f3583da3731
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-04-02 12:53:21 +0300

    feat(movie-writer): add typesense indexing for movie writers
    - include `Typesense` module in `MovieWriter` model
    - define predefined fields with references to `Movie` and `Person`
    - add attributes for indexing:
     - `id`
     - `movie_id` with dynamic retrieval
     - `person_id` with dynamic retrieval

--- app/models/movie_writer.rb
+++ app/models/movie_writer.rb
@@ -1,4 +1,22 @@
 class MovieWriter < ApplicationRecord
+  include Typesense
   belongs_to :movie
   belongs_to :person
+
+  typesense do
+    predefined_fields [
+      { "name" => "movie_id", "type" => "string", "reference" => "Movie.id" },
+      { "name" => "person_id", "type" => "string", "reference" => "Person.id" },
+    ]
+
+    attribute :id, :movie_id, :person_id
+
+    attribute :movie_id do
+      self.movie.id.to_s if self.movie.present?
+    end
+
+    attribute :person_id do
+      self.person.id.to_s if self.person
+    end
+  end
 end
