34f88da8263043603bbf232d91d309e8de2c1f66
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-04-02 15:11:45 +0300

    style(utils): format utils file based on prettier rules

--- app/frontend/lib/utils.ts
+++ app/frontend/lib/utils.ts
@@ -1,8 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import type { ClassValue } from "clsx";
+import type { SearchParams } from "typesense/lib/Typesense/Documents";
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export type SearchParamsStringMap = {
