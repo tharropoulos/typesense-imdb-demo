859e098e2525bc61cc0704162d866c32feb27341
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-04-02 13:01:28 +0300

    chore: change fs location of auth button

--- app/frontend/components/auth-button.tsx
+++ app/frontend/components/auth-button.tsx
@@ -1,26 +1,24 @@
+import type { PageProps } from "@/pages/Home";
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { router } from "@inertiajs/react";
+import { router, usePage } from "@inertiajs/react";
 
-type AuthUser = { user: string } | null;
-
-interface AuthButtonProps extends React.ComponentProps<typeof Button> {
-  auth?: AuthUser;
+interface AuthButtonProps extends React.ComponentProps<typeof Button>, PageProps {
   authVariants?: {
     signIn?: React.ComponentProps<typeof Button>["variant"];
     signOut?: React.ComponentProps<typeof Button>["variant"];
   };
 }
 
-const AuthButton: React.FC<AuthButtonProps> = ({
-  auth,
+function AuthButton({
   className,
   variant,
   authVariants = { signIn: "outline", signOut: "link" },
-  ...props
-}) => {
-  const isLoggedIn = !!auth;
+  ...rest
+}: AuthButtonProps) {
+  const { props } = usePage<AuthButtonProps>();
+  const isLoggedIn = Boolean(props.auth);
 
   const buttonVariant = variant ?? (isLoggedIn ? authVariants.signOut : authVariants.signIn);
 
@@ -33,10 +31,10 @@ const AuthButton: React.FC<AuthButtonProps> = ({
   };
 
   return (
-    <Button variant={buttonVariant} onClick={handleClick} className={cn(className)} {...props}>
+    <Button variant={buttonVariant} onClick={handleClick} className={cn(className)} {...rest}>
       {isLoggedIn ? "Sign out" : "Sign in"}
     </Button>
   );
-};
+}
 
-export default AuthButton;
+export { AuthButton };
