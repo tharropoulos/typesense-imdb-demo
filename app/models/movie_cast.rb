b85ce5b2a67efc9d4e8881cdbd76ccae777771b9
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-04-02 12:54:05 +0300

    feat(movie-cast): add typesense indexing for movie cast
    - include `Typesense` module in `MovieCast` model
    - define predefined fields with references and attributes:
     - `movie_id` referencing `Movie`
     - `person_id` referencing `Person`
     - `character_name` as searchable string
     - `role` as non-indexed string
    - add dynamic retrieval for `movie_id` and `person_id`

--- app/models/movie_cast.rb
+++ app/models/movie_cast.rb
@@ -1,4 +1,21 @@
 class MovieCast < ApplicationRecord
+  include Typesense
   belongs_to :movie
   belongs_to :person
+
+  typesense do
+    predefined_fields [
+      { "name" => "movie_id", "type" => "string", "reference" => "Movie.id" },
+      { "name" => "person_id", "type" => "string", "reference" => "Person.id" },
+      { "name" => "character_name", "type" => "string" },
+      { "name" => "role", "type" => "string", "index" => false },
+    ]
+    attribute :id, :movie_id, :person_id, :character_name, :role
+    attribute :movie_id do
+      self.movie.id.to_s if self.movie.present?
+    end
+    attribute :person_id do
+      self.person.id.to_s if self.person.present?
+    end
+  end
 end
