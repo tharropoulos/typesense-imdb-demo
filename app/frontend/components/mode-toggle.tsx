e07ca311ca81943d0cc02600436a3b850ae03550
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-04-02 13:09:18 +0300

    fix(ui): move mode toggle component from ui sub-folder to components folder

---
+++ app/frontend/components/mode-toggle.tsx
@@ -0,0 +1,21 @@
+import { useTheme } from "@/components/theme-provider";
+import { Button } from "@/components/ui/button";
+import { Moon, Sun } from "lucide-react";
+
+export function ModeToggle({ className }: { className?: string }) {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      title="Toggle theme"
+      className={className}
+    >
+      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
