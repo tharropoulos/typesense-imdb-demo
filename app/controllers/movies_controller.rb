5f4546250bfac506d41b5e96a65114ee77d884e8
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-04-02 12:35:45 +0300

    feat(movies): implement show action with movie details and breadcrumbs
    - Create `MoviesController` with `show` action
    - Use `MoviePresenter` to serialize movie data
    - Add dynamic breadcrumbs for movie detail page

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,18 @@
+class MoviesController < ApplicationController
+  include Breadcrumbs
+
+  def show
+    movie = Movie.find(params[:id])
+    all_genres = movie.genres.map { |genre| genre[:name] }.compact
+
+    primary_director = movie.movie_directors.first.person_id
+    add_breadcrumb("Movies", "/movies")
+    add_breadcrumb(movie.title)
+
+    render inertia: "Movie/Show", props: {
+             movie: MoviePresenter.new(movie).as_json,
+             breadcrumbs: @breadcrumbs,
+           }
+  end
+
+end
