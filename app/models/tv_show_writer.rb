4b6917e459bc88fd2ad9efa70f0c2bd5b3fd06b7
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-04-02 12:51:37 +0300

    feat(show-writers): add typesense indexing for tv show writers
    - include `Typesense` module in `TvShowWriter` model
    - define predefined fields with references to `Movie` and `Person`
    - add attributes for indexing:
     - `id`
     - `movie_id` with dynamic retrieval
     - `person_id` with dynamic retrieval

--- app/models/tv_show_writer.rb
+++ app/models/tv_show_writer.rb
@@ -1,4 +1,22 @@
 class TvShowWriter < ApplicationRecord
+  include Typesense
   belongs_to :tv_show
   belongs_to :person
+
+  typesense do
+    predefined_fields [
+      { "name" => "tv_show_id", "type" => "string", "reference" => "TvShow.id" },
+      { "name" => "person_id", "type" => "string", "reference" => "Person.id" },
+    ]
+
+    attribute :id, :tv_show_id, :person_id
+
+    attribute :tv_show_id do
+      self.tv_show.id.to_s if self.tv_show.present?
+    end
+
+    attribute :person_id do
+      self.person.id.to_s if self.person
+    end
+  end
 end
