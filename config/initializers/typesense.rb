9ec80fcb2b9f7dac3e7d26f04f9f801ec25f8c2d
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-04-07 12:27:52 +0300

    chore(typesense): add more number of retries and increase interval seconds

--- config/initializers/typesense.rb
+++ config/initializers/typesense.rb
@@ -3,10 +3,11 @@ Typesense.configuration = {
     host: ENV["TYPESENSE_HOST"] || "localhost",
     port: ENV["TYPESENSE_PORT"] || 8108,
     protocol: ENV["TYPESENSE_PROTOCOL"] || "http",
-    },
-  ],
+  }],
   api_key: ENV["TYPESENSE_API_KEY"] || "xyz",
   connection_timeout_seconds: 2,
+  retry_interval_seconds: 3,
+  num_retries: 5,
   logger: Logger.new($stdout),
   log_level: Logger::DEBUG,
 }
