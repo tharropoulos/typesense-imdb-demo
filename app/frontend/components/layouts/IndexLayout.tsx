0e5a92850b7816c31f0d8923327c87aa485f9eed
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-05-12 17:46:12 +0300

    refactor(layouts): create reusable index layout component
    - add generic `IndexLayout` component for media listings
    - refactor `Movie/Index.tsx` to use shared layout
    - refactor `TvShow/Index.tsx` to use shared layout
    - implement type-safe props with generics

---
+++ app/frontend/components/layouts/IndexLayout.tsx
@@ -0,0 +1,130 @@
+import type { AttributeLabel } from "@/components/instantsearch/current-refinements";
+import type { MediumType } from "@/lib/types";
+import type { PageProps } from "@/pages/Home";
+import type { BaseSearchParameters } from "typesense-instantsearch-adapter";
+import { useMemo } from "react";
+import { BreadCrumbNav } from "@/components/breadcrumbs";
+import { Filter } from "@/components/filter";
+import { CurrentRefinements } from "@/components/instantsearch/current-refinements";
+import { HitCarousel } from "@/components/instantsearch/hit";
+import { InfiniteHits } from "@/components/instantsearch/infinite-hits";
+import { RangeFilter } from "@/components/instantsearch/range-filter";
+import { RefinementList } from "@/components/instantsearch/refinement-list";
+import { mediums } from "@/lib/types";
+import { typesenseConfig } from "@/lib/typesense";
+import { InstantSearch, Index as InstantSearchIndex } from "react-instantsearch-core";
+import TypesenseInstantSearchAdapter from "typesense-instantsearch-adapter";
+
+type FilterAttribute<T extends AttributeLabel[]> = T[number]["attribute"];
+
+export interface IndexLayoutProps<T extends MediumType, A extends AttributeLabel[]> {
+  medium: T;
+  breadcrumbs: { url: string; label: string }[];
+  props: PageProps;
+  searchParams: {
+    base: BaseSearchParameters;
+    trending: BaseSearchParameters;
+    recommendations: BaseSearchParameters;
+  };
+  attributeLabels: A;
+  perPage: number;
+  rangeFilters: FilterAttribute<A>[];
+  refinements: { attribute: FilterAttribute<A>; hidden?: boolean }[];
+}
+
+export default function IndexLayout<const T extends MediumType, const A extends AttributeLabel[]>({
+  medium,
+  breadcrumbs,
+  searchParams,
+  attributeLabels,
+  perPage,
+  rangeFilters,
+  refinements,
+  props,
+}: IndexLayoutProps<T, A>) {
+  const adapter = useMemo(() => {
+    return new TypesenseInstantSearchAdapter({
+      server: typesenseConfig,
+      additionalSearchParameters: searchParams.base,
+    });
+  }, [perPage]);
+
+  const trendingAdapter = new TypesenseInstantSearchAdapter({
+    server: typesenseConfig,
+    additionalSearchParameters: searchParams.trending,
+  });
+
+  const recommendationsAdapter = useMemo(() => {
+    return new TypesenseInstantSearchAdapter({
+      server: typesenseConfig,
+      additionalSearchParameters: searchParams.recommendations,
+    });
+  }, [props.auth?.user.id]);
+
+  return (
+    <div className="mt-12 flex flex-col gap-3">
+      <div className="container mx-auto flex flex-col gap-3 px-4 pt-8">
+        <BreadCrumbNav items={breadcrumbs} />
+        <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">{mediums[medium].title}</h2>
+      </div>
+      <InstantSearch indexName={mediums[medium].indexName} searchClient={recommendationsAdapter.searchClient}>
+        <div className="w-full border-y bg-zinc-100 dark:bg-zinc-950">
+          <div className="container mx-auto flex flex-col gap-3 px-4 py-8">
+            <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">For You</h4>
+            <HitCarousel getHref={(item) => `/${medium}s/${item.id}`} />
+          </div>
+        </div>
+      </InstantSearch>
+      <InstantSearch indexName={mediums[medium].indexName} searchClient={trendingAdapter.searchClient}>
+        <div className="w-full">
+          <div className="container mx-auto flex flex-col gap-3 px-4 py-8">
+            <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">Trending</h4>
+            <HitCarousel getHref={(item) => `/${mediums[medium].route}/${item.id}`} />
+          </div>
+        </div>
+      </InstantSearch>
+
+      <div id="all-movies">
+        <InstantSearch
+          indexName={mediums[medium].indexName}
+          searchClient={adapter.searchClient}
+          future={{
+            preserveSharedStateOnUnmount: true,
+          }}
+          routing={true}
+        >
+          <InstantSearchIndex indexName={mediums[medium].indexName} indexId="all-movies">
+            <div className="container mx-auto flex flex-col gap-3 px-4 py-8">
+              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">All {mediums[medium].title}</h4>
+              <CurrentRefinements attributeLabels={attributeLabels} />
+              <Filter>
+                <div className="flex justify-between gap-2">
+                  <div className="flex w-2/3 flex-col gap-2">
+                    {refinements.map((attribute) => {
+                      const label = attributeLabels.find((label) => label.attribute === attribute.attribute)?.label;
+                      return (
+                        <RefinementList
+                          key={`${attribute.attribute}-refinement`}
+                          attribute={attribute.attribute}
+                          label={label ?? attribute.attribute}
+                          className={attribute.hidden ? "hidden" : ""}
+                        />
+                      );
+                    })}
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    {rangeFilters.map((attribute) => {
+                      const label = attributeLabels.find((label) => label.attribute === attribute)?.label;
+                      return <RangeFilter key={attribute} attribute={attribute} label={label ?? attribute} />;
+                    })}
+                  </div>
+                </div>
+              </Filter>
+              <InfiniteHits getHref={(item) => `/${mediums[medium].route}/${item.id}`} />
+            </div>
+          </InstantSearchIndex>
+        </InstantSearch>
+      </div>
+    </div>
+  );
+}
--- app/frontend/pages/Movie/Index.tsx
+++ app/frontend/pages/Movie/Index.tsx
@@ -1,18 +1,8 @@
 import type { PageProps } from "@/pages/Home";
 import type { BaseSearchParameters } from "typesense-instantsearch-adapter";
-import { useMemo } from "react";
-import { BreadCrumbNav } from "@/components/breadcrumbs";
-import { Filter } from "@/components/filter";
-import { CurrentRefinements } from "@/components/instantsearch/current-refinements";
-import { HitCarousel } from "@/components/instantsearch/hit";
-import { InfiniteHits } from "@/components/instantsearch/infinite-hits";
-import { RangeFilter } from "@/components/instantsearch/range-filter";
-import { RefinementList } from "@/components/instantsearch/refinement-list";
+import IndexLayout from "@/components/layouts/IndexLayout";
 import { useMediaQueries } from "@/hooks/use-media-queries";
-import { typesenseConfig } from "@/lib/typesense";
 import { usePage } from "@inertiajs/react";
-import { InstantSearch, Index as InstantSearchIndex } from "react-instantsearch-core";
-import TypesenseInstantSearchAdapter from "typesense-instantsearch-adapter";
 
 export default function Index({
   breadcrumbs,
@@ -35,102 +25,45 @@ export default function Index({
     : isMobile ? 6
     : 4;
 
-  const adapter = useMemo(() => {
-    return new TypesenseInstantSearchAdapter({
-      server: typesenseConfig,
-      additionalSearchParameters: {
-        query_by: "title, description",
-        query_by_weights: "10,1",
-        per_page: perPage,
-        exclude_fields: "user_embedding, item_embedding, embedding",
-      },
-    });
-  }, [perPage]);
-
-  const trendingAdapter = new TypesenseInstantSearchAdapter({
-    server: typesenseConfig,
-    additionalSearchParameters: {
-      query_by: "title, description",
-      sort_by:
-        "_eval([(num_votes:>3000):5, (average_rating:>7.0):2, (average_rating:>6.0):3, (average_rating:<5.0):1]):desc, release_year:desc, num_votes:desc",
-      perPage: 15,
-      exclude_fields: "user_embedding, item_embedding, embedding",
-    },
-  });
-
-  const recommendationsAdapter = useMemo(() => {
-    return new TypesenseInstantSearchAdapter({
-      server: typesenseConfig,
-      additionalSearchParameters: {
-        preset: "movie_recommendations",
-        personalization_user_id: props.auth?.user.id.toString(),
-        exclude_fields: "user_embedding, item_embedding, embedding",
-      },
-    });
-  }, [props.auth?.user.id]);
-
   return (
-    <div className="mt-12 flex flex-col gap-3">
-      <div className="container mx-auto flex flex-col gap-3 px-4 pt-8">
-        <BreadCrumbNav items={breadcrumbs} />
-        <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">Movies</h2>
-      </div>
-      <InstantSearch indexName="Movie" searchClient={recommendationsAdapter.searchClient}>
-        <div className="w-full border-y bg-zinc-100 dark:bg-zinc-950">
-          <div className="container mx-auto flex flex-col gap-3 px-4 py-8">
-            <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">For You</h4>
-            <HitCarousel getHref={(movie) => `/movies/${movie.id}`} />
-          </div>
-        </div>
-      </InstantSearch>
-      <InstantSearch indexName="Movie" searchClient={trendingAdapter.searchClient}>
-        <div className="w-full">
-          <div className="container mx-auto flex flex-col gap-3 px-4 py-8">
-            <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">Trending</h4>
-            <HitCarousel getHref={(movie) => `/movies/${movie.id}`} />
-          </div>
-        </div>
-      </InstantSearch>
-
-      <div id="all-movies">
-        <InstantSearch
-          indexName="Movie"
-          searchClient={adapter.searchClient}
-          future={{
-            preserveSharedStateOnUnmount: true,
-          }}
-          routing={true}
-        >
-          <InstantSearchIndex indexName="Movie" indexId="all-movies">
-            <div className="container mx-auto flex flex-col gap-3 px-4 py-8">
-              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">All Movies</h4>
-              <CurrentRefinements
-                attributeLabels={[
-                  { attribute: "genres", label: "Genre" },
-                  { attribute: "release_year", label: "Release Year" },
-                  { attribute: "average_rating", label: "Rating" },
-                  { attribute: "directors", label: "Director" },
-                  { attribute: "countries", label: "Country" },
-                ]}
-              />
-              <Filter>
-                <div className="flex justify-between gap-2">
-                  <div className="flex w-2/3 flex-col gap-2">
-                    <RefinementList attribute="genres" label="Genres" />
-                    <RefinementList attribute="directors" label="Directors" className="hidden" />
-                    <RefinementList attribute="countries" label="Country" className="" />
-                  </div>
-                  <div className="flex flex-col gap-2">
-                    <RangeFilter attribute="release_year" label="Release Year" />
-                    <RangeFilter attribute="average_rating" label="Rating" />
-                  </div>
-                </div>
-              </Filter>
-              <InfiniteHits getHref={(movie) => `/movies/${movie.id}`} />
-            </div>
-          </InstantSearchIndex>
-        </InstantSearch>
-      </div>
-    </div>
+    <IndexLayout
+      props={props}
+      searchParams={{
+        base: {
+          query_by: "title, description",
+          query_by_weights: "10,1",
+          per_page: perPage,
+          exclude_fields: "user_embedding, item_embedding, embedding",
+        },
+        trending: {
+          query_by: "title, description",
+          sort_by:
+            "_eval([(num_votes:>3000):5, (average_rating:>7.0):2, (average_rating:>6.0):3, (average_rating:<5.0):1]):desc, release_year:desc, num_votes:desc",
+          perPage: 15,
+          exclude_fields: "user_embedding, item_embedding, embedding",
+        },
+        recommendations: {
+          preset: "movie_recommendations",
+          personalization_user_id: props.auth?.user.id.toString(),
+          exclude_fields: "user_embedding, item_embedding, embedding",
+        },
+      }}
+      perPage={perPage}
+      medium="movie"
+      breadcrumbs={breadcrumbs}
+      attributeLabels={[
+        { attribute: "genres", label: "Genre" },
+        { attribute: "release_year", label: "Release Year" },
+        { attribute: "average_rating", label: "Rating" },
+        { attribute: "directors", label: "Director" },
+        { attribute: "countries", label: "Country" },
+      ]}
+      rangeFilters={["release_year", "average_rating"]}
+      refinements={[
+        { attribute: "genres" },
+        { attribute: "directors", hidden: true },
+        { attribute: "countries"},
+      ]}
+    />
   );
 }
