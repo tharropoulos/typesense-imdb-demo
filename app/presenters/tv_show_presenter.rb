5c51fe9fc9ed2127df4f6c6c1a30019f8936c052
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-05-13 12:48:37 +0300

    refactor(tv_shows): restructure tv show data presentation
    - remove `runtime_minutes` from tv show json attributes
    - add explicit `collection_type` to tv show json
    - refactor `TvShowPresenter#as_json` to use direct mapping for associations
    - align tv show presenter with movie presenter implementation

--- app/presenters/tv_show_presenter.rb
+++ app/presenters/tv_show_presenter.rb
@@ -5,24 +5,46 @@ class TvShowPresenter
 
   def as_json
     base_json = @tv_show.as_json(
-      only: [:id, :title, :start_year, :end_year, :primary_image_url, :description, :runtime_minutes, :average_rating, :content_rating, :total_episodes, :total_seasons],
+      only: [:id, :title, :start_year, :end_year, :primary_image_url, :description, :average_rating, :content_rating, :total_episodes, :total_seasons],
       methods: [:countries, :genres, :reviews],
     )
 
-    base_json["casts"] = @tv_show.tv_show_casts.as_json(
-      only: [:id, :character_name, :role],
-      include: { person: { only: [:id, :full_name, :url] } },
-    )
+    base_json["collection_type"] = "tv_show"
 
-    base_json["directors"] = @tv_show.tv_show_directors.as_json(
-      only: [:id],
-      include: { person: { only: [:id, :full_name, :url] } },
-    )
+    base_json["casts"] = @tv_show.cast_members.map do |cast|
+      {
+        id: cast.id,
+        character_name: cast.tv_show_casts.find_by(tv_show_id: @tv_show.id)&.character_name,
+        role: cast.tv_show_casts.find_by(tv_show_id: @tv_show.id)&.role,
+        person: {
+          id: cast.id,
+          full_name: cast.full_name,
+          url: cast.url,
+        },
+      }
+    end
 
-    base_json["writers"] = @tv_show.tv_show_writers.as_json(
-      only: [:id],
-      include: { person: { only: [:id, :full_name, :url] } },
-    )
+    base_json["directors"] = @tv_show.directors.map do |director|
+      {
+        id: director.id,
+        person: {
+          id: director.id,
+          full_name: director.full_name,
+          url: director.url,
+        },
+      }
+    end
+
+    base_json["writers"] = @tv_show.writers.map do |writer|
+      {
+        id: writer.id,
+        person: {
+          id: writer.id,
+          full_name: writer.full_name,
+          url: writer.url,
+        },
+      }
+    end
 
     base_json
   end
